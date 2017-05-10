cf4e3533fac092684962d7766036059f2f1e77e8
Richard Silverton <silverton.richard@googlemail.com>
2017-05-10 09:16:51 +0100

    Removes rebass

--- components/Player.js
+++ components/Player.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Card, CardImage, Heading, Text } from 'rebass'
 import Link from 'next/link'
 import { popMessage } from '../redux/actions'
 
